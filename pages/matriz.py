3e998e244e3305f3d2d5f9488feb8c8d0eb6b5dc
Ivanjr8 <ivanjr8@hotmail.com>
2025-08-29 12:13:44 -0300

    1
    1

--- pages/matriz.py
+++ pages/matriz.py
@@ -182,6 +182,44 @@ if conn and engine:
                     "id_modulo": id_modulo,
                     "acesso": chave
                 })
-
-    if st.button("💾 Salvar Acessos"):
-        salvar_acessos(acessos_atualizados, df_acesso, cursor, conn)
+# 🧭 Barra lateral personalizada
+with st.sidebar:
+        if "usuario" in st.session_state and "perfil" in st.session_state:
+            st.markdown(f"""
+            👋 Olá, **{st.session_state.usuario}**  
+            🔐 Perfil: **{st.session_state.perfil}**
+            """)
+        st.markdown("## 🧭 Navegação")
+        if st.button("🎓   Chatbot", key="btn_chatbot"):
+            st.switch_page("pages/chatbot.py")
+        if st.button("🖥️   Gerar Simulado", key="btn_simulado"):
+            st.switch_page("pages/Gerar_Simulado.py")
+        if st.button("✅   Teste de Conexão", key="btn_azure"):
+            st.switch_page("pages/conn_azure.py")
+        if st.button("↩️   Retornar", key="btn_retornar"):
+            st.switch_page("gemini.py")
+        st.markdown("---")
+        st.markdown("## ⚙️   Cadastro")
+        if st.button("🗂️   Questões", key="btn_cadastrar"):
+            st.switch_page("pages/Cadastrar_Questões.py")
+        if st.button("🗂️   Respostas", key="btn_cadastrar_respostas"):
+            st.switch_page("pages/Cadastrar_Respostas.py")
+        if st.button("🗂️   Cadastrar Usuários", key="btn_cadastrar_usuarios"):
+            st.switch_page("pages/Cadastrar_Usuarios.py")
+            st.markdown("---")
+        
+        st.markdown("---")
+        st.markdown("### 📞   Suporte")
+        st.write("Email: suporte@meuapp.com")
+        
+        # Botão para sair
+        if st.button("🚪 Sair"):
+        # Remove dados de sessão
+            for key in ["usuario", "perfil", "usuario_id"]:
+                st.session_state.pop(key, None)
+        # Redireciona para a página inicial (gemini.py)
+                st.switch_page("gemini.py")
+            # Reinicia a aplicação
+                st.rerun()
+            if st.button("💾 Salvar Acessos"):
+                salvar_acessos(acessos_atualizados, df_acesso, cursor, conn)
